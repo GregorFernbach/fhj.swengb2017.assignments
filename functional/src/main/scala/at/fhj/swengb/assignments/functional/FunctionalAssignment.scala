40e021cd1b6459dadb1d0d23d57b811760664a9c
XPS13-9360\Gregor Fernbach <gregor.fernbach.2@mailbox.org>
2017-11-23 12:00:38 +0100

    Functional Assignment

--- functional/src/main/scala/at/fhj/swengb/assignments/functional/FunctionalAssignment.scala
+++ functional/src/main/scala/at/fhj/swengb/assignments/functional/FunctionalAssignment.scala
@@ -9,12 +9,12 @@ object FunctionalAssignment {
   /**
     * A function which returns its parameters in a changed order. Look at the type signature.
     */
-  def flip[A, B](t: (A, B)): (B, A) = ???
+  def flip[A, B](t: (A, B)): (B, A) = (t._2, t._1)
 
   /**
     * given a Seq[A] and a function f : A => B, return a Seq[B]
     */
-  def unknown[A, B](as: Seq[A], fn: A => B): Seq[B] = ???
+  def unknown[A, B](as: Seq[A], fn: A => B): Seq[B] = as.map(fn)
 
   /**
     * Returns the absolute value of the parameter i.
@@ -22,8 +22,7 @@ object FunctionalAssignment {
     * @param i a value, either with a positive or a negative sign.
     * @return
     */
-  def abs(i: Int): Int = ???
-
+  def abs(i: Int): Int = if (i < 0) -1 * i else i
 
   // Describe with your own words what this function does.
   // in the comment below, add a description what this function does - in your own words - and give
@@ -34,12 +33,12 @@ object FunctionalAssignment {
   //
   /**
     *
-    * @param as
-    * @param b
-    * @param fn
-    * @tparam A
-    * @tparam B
-    * @return
+    * @param as: this is a Sequence of type A
+    * @param b: this is something of type B
+    * @param fn: This is a function that take a tuple of (type B, type A) and returns a value of type B
+    * @tparam A: a value of different data type than B
+    * @tparam B: a value of different data type than A
+    * @return: the return type is of type B
     */
   def op[A, B](as: Seq[A], b: B)(fn: (B, A) => B): B = as.foldLeft(b)(fn)
 
@@ -50,8 +49,7 @@ object FunctionalAssignment {
     * @param numbers
     * @return
     */
-  def sum(numbers: Seq[Int]): Int = ???
-
+  def sum(numbers: Seq[Int]): Int = op(numbers, 0)(_ + _)
 
   /**
     * calculate the factorial number of the given parameter.
@@ -63,7 +61,7 @@ object FunctionalAssignment {
     * @param i parameter for which the factorial must be calculated
     * @return i!
     */
-  def fact(i: Int): Int = ???
+  def fact(i: Int): Int = if (i == 0) 1 else i * fact(i - 1)
 
   /**
     * compute the n'th fibonacci number
@@ -73,7 +71,11 @@ object FunctionalAssignment {
     *
     * https://en.wikipedia.org/wiki/Fibonacci_number
     */
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = n match {
+    case 0 => 0
+    case 1 => 1
+    case _ => fib(n - 1) + fib(n - 2)
+  }
 
   /**
     * Implement a isSorted which checks whether an Array[A] is sorted according to a
@@ -83,6 +85,7 @@ object FunctionalAssignment {
     * Elements which are equal are considered to be ordered.
     */
   def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = ???
+  //AND (for ( x <- as.grouped(2)) yield x.map(gt))
 
   /**
     * Takes both lists and combines them, element per element.
@@ -90,7 +93,7 @@ object FunctionalAssignment {
     * If one sequence is shorter than the other one, the function stops at the last element
     * of the shorter sequence.
     */
-  def genPairs[A, B](as: Seq[A], bs: Seq[B]): Seq[(A, B)] = ???
+  def genPairs[A, B](as: Seq[A], bs: Seq[B]): Seq[(A, B)] = as zip bs
 
   // a simple definition of a linked list, we define our own list data structure
   sealed trait MyList[+A]
@@ -103,18 +106,21 @@ object FunctionalAssignment {
   // it also provides a convenience constructor in order to instantiate a MyList without hassle
   object MyList {
 
-    def sum[Int](list: MyList[Int]): Int = ???
+    def sum(list: MyList[Int]): Int = list match {
+      case MyNil            => 0
+      case Cons(head, tail) => head + sum(tail)
+    }
 
-    def product[Int](list: MyList[Int]): Int = ???
+    def product(list: MyList[Int]): Int = list match {
+      case MyNil            => 1
+      case Cons(head, tail) => head * product(tail)
+    }
 
     def apply[A](as: A*): MyList[A] = {
-      as match {
-        case Nil => MyNil
-        case h :: tl => Cons(h, apply(tl: _*))
-      }
+      if (as.isEmpty) MyNil
+      else Cons(as.head, apply(as.tail: _*))
     }
 
   }
 
 }
-
